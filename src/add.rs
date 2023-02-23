f8c3084f69f3a86caa2b18ac8bdbf33762fe4818
taro <teroro1226@outlook.jp>
2023-02-24 04:46:48 +0900

    feat: add 'add' func

--- src/add.rs
+++ src/add.rs
@@ -1,7 +1,16 @@
+use std::fs::File;
+use std::io::Result;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 pub struct AddOptions {
     #[structopt(long)]
-    file: String
+    file: String,
+}
+
+pub fn add(args: &AddOptions) -> Result<()> {
+    let folder = "./files/";
+    let path = folder.to_string() + &args.file;
+    File::create(path).expect("failed to create file");
+    Ok(())
 }
