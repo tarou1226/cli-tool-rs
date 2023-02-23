40ff214389b2ff9c6f74e1d82809a16641104b9b
taro <teroro1226@outlook.jp>
2023-02-24 04:48:21 +0900

    feat: add conditional branch process

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use cli_tools::cli::Cli;
+use cli_tools::add::add;
+use cli_tools::cli::{Cli, Command};
 use structopt::StructOpt;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let args = Cli::from_args();
+    match args.cmd {
+        Command::Add(args) => add(&args),
+    }
 }
