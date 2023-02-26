58b687eaae6e3294f8ff6ea95170479f5009d5c7
taro <teroro1226@outlook.jp>
2023-02-26 21:21:46 +0900

    feat: add conditional branch process for List

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use cli_tools::add::add;
 use cli_tools::cli::{Cli, Command};
+use cli_tools::list::list;
 use structopt::StructOpt;
 
 fn main() -> Result<(), std::io::Error> {
     let args = Cli::from_args();
+    let folder = "./files/";
     match args.cmd {
-        Command::Add(args) => add(&args),
+        Command::Add(args) => add(folder, &args),
+        Command::List(args) => list(folder, &args),
     }
 }
